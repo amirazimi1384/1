c55975262d2038ddd4b7eddf9655deb824c42b09
amirazimi1384 <amirazimi309@gmail.com>
2024-12-06 01:04:50 +0330

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Insert Page</title>
-        <link rel="stylesheet" href="all4.css">
+        <link rel="stylesheet" href="all5.css">
     </head>
         <body>
         <div>
